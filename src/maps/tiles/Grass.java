c0dea09d9275635077c8a8c8b4e96a2834ff019d
charliegregg <charliejamesgregg@gmail.com>
2023-08-06 22:17:10 +1000

    need to fix textures

--- src/maps/tiles/Grass.java
+++ src/maps/tiles/Grass.java
@@ -1,7 +1,22 @@
 package maps.tiles;
 
+import java.awt.Graphics2D;
+
 import maps.Tile;
+import maps.Vector2;
 
 public class Grass extends Tile {
+
+    @Override
+    public void update(double deltaTime) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void draw(Graphics2D g, Vector2 offset) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
     
 }
